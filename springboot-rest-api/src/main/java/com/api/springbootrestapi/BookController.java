ea4b129ce497e46aeadf9949c2b1c245b08756d6
Amarjeet <kumaramarjeet001@gmail.com>
2023-11-11 16:23:47 +0530

    * spring boot rest api project * Created get books

---
+++ springboot-rest-api/src/main/java/com/api/springbootrestapi/BookController.java
@@ -0,0 +1,17 @@
+package com.api.springbootrestapi;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BookController {
+    
+    @RequestMapping(value="/books",method = RequestMethod.GET)
+    @ResponseBody
+    public String getBooks()
+    {
+        return  "This is our first rest API project" ;
+    }
+
+}
