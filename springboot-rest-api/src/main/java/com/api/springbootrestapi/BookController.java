6914a24e6499f7bc510bfc262235eaea1cb0a52d
Amarjeet <kumaramarjeet001@gmail.com>
2023-11-11 17:52:20 +0530

    * Handled get request from postman  in REST API * Send request in json format

--- springboot-rest-api/src/main/java/com/api/springbootrestapi/BookController.java
+++ springboot-rest-api/src/main/java/com/api/springbootrestapi/BookController.java
@@ -1,17 +1,52 @@
 package com.api.springbootrestapi;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.api.springbootrestapi.entities.Book;
+import com.api.springbootrestapi.service.BookService;
+import java.util.* ;
 
-@Controller
+//  Rest controller so that response will not go in html page
+@RestController
 public class BookController {
     
+    //  Autowiring to create an object by spring
+    @Autowired
+    private BookService bookService ;
+
     @RequestMapping(value="/books",method = RequestMethod.GET)
+    //  Send the response to the client as it is returning like no html will be returned
     @ResponseBody
-    public String getBooks()
+    public String firstBook()
     {
         return  "This is our first rest API project" ;
+    }
+
+    //  Get single book
+    //  Get Mapping is a combination of request mapping with type get
+    @GetMapping("/get-books")
+    public Book getBook()
+    {
+        Book book = new Book(1,"Death","Shankara") ;
+        return book ;
+    }
+
+    @GetMapping("/get-single-book")
+    public Book getSingleBook()
+    {
+       Book book = bookService.getSingleBook(1) ;
+        return book ;
+    }
+
+    @GetMapping("/get-all-book")
+    public List<Book> getAllBooks()
+    {
+        List<Book> books = bookService.getAllBook() ;
+        return books ;
     }
 
 }
---
+++ springboot-rest-api/src/main/java/com/api/springbootrestapi/service/BookService.java
@@ -0,0 +1,30 @@
+package com.api.springbootrestapi.service;
+
+import java.util.*;
+import java.util.stream.Collectors;  
+import org.springframework.stereotype.Component;
+
+import com.api.springbootrestapi.entities.Book;
+
+@Component
+public class BookService {
+    private static List<Book> books = new ArrayList() ;
+
+    static{
+        books.add(new Book(1,"Java Fundamentals","Manish"));
+        books.add(new Book(2,"C++ Fundamentals","Sukesh")) ;
+        books.add(new Book(3,"PHP Fundamentals","Arvina")) ;
+    }
+
+    public Book getSingleBook(int id)
+    {
+        //  Compare object id in a list and get single book object
+        Book book = books.stream().filter(e->e.getId() == id).findFirst().get() ;
+        return book ;
+    }
+
+    public List<Book> getAllBook()
+    {
+        return books ;
+    }
+}
