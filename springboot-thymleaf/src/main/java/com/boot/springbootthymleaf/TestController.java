5b39a4725fc37c697dcfc03df8a3ac664d6bc4cf
Amarjeet <kumaramarjeet001@gmail.com>
2023-11-26 11:08:27 +0530

    * Thymleaf project initialized

---
+++ springboot-thymleaf/src/main/java/com/boot/springbootthymleaf/TestController.java
@@ -0,0 +1,16 @@
+package com.boot.springbootthymleaf ;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+    
+    @GetMapping("/test")
+    public String index(){
+        System.out.println("hello world");
+        return "test" ;
+    }
+
+    
+
+}
---
+++ springboot-thymleaf/src/test/java/com/boot/springbootthymleaf/SpringbootThymleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.springbootthymleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootThymleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
