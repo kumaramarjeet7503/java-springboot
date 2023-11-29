6078a62a8555b6fb268991704b472d4286b09445
Amarjeet <kumaramarjeet001@gmail.com>
2023-11-29 23:02:16 +0530

    * Project setup smart contact manager

---
+++ smart-manager-spring-security/src/main/java/com/contact/smartmanagerspringsecurity/SmartManagerSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.contact.smartmanagerspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartManagerSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartManagerSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ smart-manager-spring-security/src/test/java/com/contact/smartmanagerspringsecurity/SmartManagerSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contact.smartmanagerspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartManagerSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
