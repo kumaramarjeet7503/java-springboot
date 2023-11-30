59ec407e5ed75ad553e4c41371aa6c473bdfb15a
Amarjeet <kumaramarjeet001@gmail.com>
2023-11-30 23:18:43 +0530

    * Setting up Bootstrap and Templates

--- smart-manager-spring-security/src/main/java/com/contact/smartmanagerspringsecurity/controllers/HomeController.java
+++ smart-manager-spring-security/src/main/java/com/contact/smartmanagerspringsecurity/controllers/HomeController.java
@@ -2,6 +2,7 @@ package com.contact.smartmanagerspringsecurity.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.contact.smartmanagerspringsecurity.dao.UserRepository;
@@ -23,4 +24,17 @@ public class HomeController {
         return "welcome" ;
     }
 
+    @GetMapping("/home")
+    public String home(Model m)
+    {
+        m.addAttribute("title","Smart Contact : Home") ;
+        return "home" ;
+    }
+
+        @GetMapping("/about")
+    public String about(Model m)
+    {
+        m.addAttribute("title","Smart Contact : About") ;
+        return "about" ;
+    }
 }
