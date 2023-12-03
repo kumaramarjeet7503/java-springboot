20df1d103d2791bf6bf1f66657fd7986dbb572a8
Amarjeet <kumaramarjeet001@gmail.com>
2023-12-03 21:16:26 +0530

    * User dashboard integration navbar setup

--- smart-manager-spring-security/src/main/java/com/contact/smartmanagerspringsecurity/controllers/UserController.java
+++ smart-manager-spring-security/src/main/java/com/contact/smartmanagerspringsecurity/controllers/UserController.java
@@ -1,15 +1,30 @@
 package com.contact.smartmanagerspringsecurity.controllers;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.contact.smartmanagerspringsecurity.dao.UserRepository;
+import com.contact.smartmanagerspringsecurity.entitity.User;
+
+import ch.qos.logback.core.model.Model;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
     
+    @Autowired
+    private UserRepository userRepository ;
+
     @RequestMapping("index")
-    public String index()
+    public String index(org.springframework.ui.Model model,Principal principal)
     {
+        String userName = principal.getName() ;
+        User user = this.userRepository.getUserByUserName(userName) ;
+        model.addAttribute("user",user) ;
+        System.out.println(user);
         return "user_dashboard" ;
     }
 }
