4cb8cacd7fe916aca0fe7e84e70fd18dca2df8cd
Amarjeet <kumaramarjeet001@gmail.com>
2023-12-04 23:44:27 +0530

    * File upload handling from form

--- smart-manager-spring-security/src/main/java/com/contact/smartmanagerspringsecurity/controllers/UserController.java
+++ smart-manager-spring-security/src/main/java/com/contact/smartmanagerspringsecurity/controllers/UserController.java
@@ -1,12 +1,21 @@
 package com.contact.smartmanagerspringsecurity.controllers;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.jpa.domain.JpaSort.Path;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.ui.Model ;
 import com.contact.smartmanagerspringsecurity.dao.UserRepository;
 import com.contact.smartmanagerspringsecurity.entitity.Contact;
@@ -45,9 +54,18 @@ public class UserController {
     }
 
     @PostMapping("process-contact")
-    public String processContact(@ModelAttribute("contact") Contact contact, Model model){
+    public String processContact(@ModelAttribute("contact") Contact contact,@RequestParam("profileImage") MultipartFile file , Model model) throws IOException{
         User user = (User) model.getAttribute("user") ;
         contact.setUser(user);
+        //  File uploading with file copy function
+        if(!file.isEmpty())
+        {
+            contact.setImage(file.getOriginalFilename());
+            File saveFile = new ClassPathResource("static/images").getFile() ;
+            java.nio.file.Path path = Paths.get(saveFile.getAbsolutePath()+File.separator+file.getOriginalFilename()) ;
+            Files.copy(file.getInputStream(),path,StandardCopyOption.REPLACE_EXISTING) ;
+        }
+
         user.getContacts().add(contact) ;
         this.userRepository.save(user) ;
         return "add_contact" ;
