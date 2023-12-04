20d29a4b415e4dede8ee1212ff4d125a57079e6f
Amarjeet <kumaramarjeet001@gmail.com>
2023-12-04 23:25:05 +0530

    * Sidebar UI , Add contact functionality

--- smart-manager-spring-security/src/main/java/com/contact/smartmanagerspringsecurity/controllers/UserController.java
+++ smart-manager-spring-security/src/main/java/com/contact/smartmanagerspringsecurity/controllers/UserController.java
@@ -4,12 +4,14 @@ import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.ui.Model ;
 import com.contact.smartmanagerspringsecurity.dao.UserRepository;
+import com.contact.smartmanagerspringsecurity.entitity.Contact;
 import com.contact.smartmanagerspringsecurity.entitity.User;
 
-import ch.qos.logback.core.model.Model;
 
 @Controller
 @RequestMapping("user")
@@ -18,13 +20,36 @@ public class UserController {
     @Autowired
     private UserRepository userRepository ;
 
-    @RequestMapping("index")
-    public String index(org.springframework.ui.Model model,Principal principal)
+    @ModelAttribute
+    public void getUser(Model model,Principal principal)
     {
         String userName = principal.getName() ;
         User user = this.userRepository.getUserByUserName(userName) ;
         model.addAttribute("user",user) ;
-        System.out.println(user);
+    }
+
+    @RequestMapping("index")
+    public String index(org.springframework.ui.Model model,Principal principal)
+    {
+        model.addAttribute("title", "dashboard") ;
         return "user_dashboard" ;
+    }
+
+    @RequestMapping("add-contact")
+    public String addContact(org.springframework.ui.Model model)
+    {
+        Contact contact = new Contact() ;
+        model.addAttribute("title", "Add contact") ;
+        model.addAttribute("contact", contact) ;
+        return "add_contact" ;
+    }
+
+    @PostMapping("process-contact")
+    public String processContact(@ModelAttribute("contact") Contact contact, Model model){
+        User user = (User) model.getAttribute("user") ;
+        contact.setUser(user);
+        user.getContacts().add(contact) ;
+        this.userRepository.save(user) ;
+        return "add_contact" ;
     }
 }
---
+++ smart-manager-spring-security/src/main/resources/static/js/base.js
@@ -0,0 +1,13 @@
+function manageNav(){
+    let sidebar = document.getElementById('sidebar') ;
+    sidebar.style.display = sidebar.style.display == 'none' ? 'block' : 'none' ;
+    let reopen =  document.getElementById('reopen') ;
+    let content =  document.getElementById('content') ;
+    if( sidebar.style.display == 'none' )
+    {
+        content.style.marginLeft = '2%' ;
+       
+    }else{
+        content.style.marginLeft = '17%' ;
+    }
+}
