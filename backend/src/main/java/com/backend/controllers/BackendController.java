887d37eace8ef4175096ea16682fdebe02a03c53
Amarjeet <kumaramarjeet001@gmail.com>
2024-03-18 21:29:47 +0530

    ci(auth) : api authentication

--- backend/src/main/java/com/backend/controllers/BackendController.java
+++ backend/src/main/java/com/backend/controllers/BackendController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 public class BackendController {
@@ -11,11 +14,19 @@ public class BackendController {
     @Autowired
     private Environment env;
 
-    @GetMapping("/welcome")
+    @GetMapping("/get/welcome")
     public String welcome()
     {
         String url = env.getProperty("backend.url") ;
         return url ;
     }
+
+    
+	@PostMapping("/post/welcome")
+	public String postMethodName( ) {
+        		
+		return "This is the post request";
+	}
+	
 
 }
